e5954338a1f4a48120c3d79d80eecd3922ba4395
Faiq Ahmad <64751373+realfaiq@users.noreply.github.com>
2021-12-06 02:36:04 +0500

    SignUp Page Added

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,49 +1,78 @@
 import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link} from "react-router-dom";
+
 
 class SignUp extends Component {
     render() {
         return (
-            <div class="modal fade" id="signupModal">
-                <div class="modal-dialog modal-lg">
-                    <div class="modal-content">
-                        <div class="modal-header bg-primary text-white">
-                            <h5 class="modal-title">Sign Up to <strong>Discussions</strong></h5>
-                            <button class="close" data-dismiss="modal">
-                                <span class="text-white">&times;</span>
-                            </button>
+    
+    <div class="d-lg-flex half">
+        <div class="bg" style={{ backgroundImage: "url(" + "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" + ")" }}></div>
+        
+        <div className="content m-0 p-0">
+            <div className="container ml-sm-5 mt-5">
+                <h4 className="text-center ml-sm-5">Sign Up to<strong> Discussion Forums</strong></h4>
+
+                
+                <form action="#" method="POST" class="mt-5">
+
+                    <div class="form-row mb-3">
+                        <div class="col m-0 p-0 ml-sm-5">
+                          <input type="text" class="form-control" id="firstname" placeholder="First name"></input>
                         </div>
-                        <div class="modal-body">
-                            <div class="container">
-                                <form action="#" method="post">
-                                    <div class="form-group">
-                                        <label for="FullName">Full Name</label>
-                                        <input type="text" class="form-control" name="fullname" placeholder="Enter Your Full Name" />
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="email">Email</label>
-                                        <input type="email" class="form-control" name="email" placeholder="your-email@gmail.com" />
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="password">Password</label>
-                                        <input type="password" class="form-control" name="password" placeholder="Password" />
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="cpassword">Confirm Password</label>
-                                        <input type="password" class="form-control" name="cpassword" placeholder="Confirm Password" />
-                                    </div>
-
-                                    <div class="form-group">
-                                        <input type="submit" value="Sign Up" class="form-control btn btn-primary" name="submit" />
-                                    </div>
-                                </form>
-                            </div>
+                        <div class="col">
+                          <input type="text" class="form-control" id="lastname" placeholder="Last name"></input>
                         </div>
+                      </div>
+
+                    <div class="input-group mb-3 ml-sm-4 justify-content-center">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="basic-addon1">@</span>
+                        </div>
+                        <input type="text" id="user" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"></input>
                     </div>
-                </div>
+
+                    <div class="input-group mb-3 ml-sm-4 justify-content-center">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="basic-addon1"><i class="far fa-envelope-open"></i></span>
+                        </div>
+                        <input type="email" id="address" class="form-control" placeholder="yourname@gmail.com" aria-label="Username" aria-describedby="basic-addon1"></input>
+                    </div>
+
+                    <div class="input-group mb-3 ml-sm-4 justify-content-center">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="basic-addon1"><i class="fas fa-unlock-alt"></i></span>
+                        </div>
+                        <input type="password" class="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1"></input>
+                    </div>
+
+                    <div class="input-group mb-3 ml-sm-4 justify-content-center">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="basic-addon1"><i class="fas fa-unlock-alt"></i></span>
+                        </div>
+                        <input type="password" class="form-control" placeholder="Confirm Password" aria-label="Username" aria-describedby="basic-addon1"></input>
+                    </div>
+
+                    <div class="input-group mb-3 ml-sm-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fas fa-birthday-cake"></i></span>
+                        </div>
+                        <input type="date" id="birthday" name="birthday" class="form-control"></input>
+                    </div>
+
+
+                    <input type="submit" value="Log In" class="btn btn-block btn-primary ml-4"/>
+
+                </form>
+
             </div>
+        </div>
+
+    </div>
+
+    
+
         )
     }
 }

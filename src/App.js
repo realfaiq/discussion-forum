5634150535e69c97c00852964682bcb46c0be550
Faiq Ahmad <64751373+realfaiq@users.noreply.github.com>
2021-12-06 21:21:08 +0500

    Retrieving the form Data from the Inputs for Login Page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,26 @@ import {Link} from "react-router-dom";
 
 
 class App extends Component{
+
+    state = {
+      username: '',
+      password: ''
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    var username = this.state.username;
+    var password = this.state.password;
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name] : e.target.value});
+
+  }
+
+  
+  
+
   render (){
     return (
 
@@ -14,6 +34,7 @@ class App extends Component{
       //   <Header/>
       //   <Contact name="Asim" email="de.asimhabib@gmail.com" />
       // </div>
+    <div>
     <div class="d-lg-flex half">
     <div class="bg order-1 order-md-2" style={{ backgroundImage: "url(" + "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" + ")" }}>
     </div>
@@ -24,14 +45,14 @@ class App extends Component{
           <div class="col-md-7">
             <h3>Login to <strong>Discussion</strong></h3>
             <p class="mb-4">Join the most influential discussion forum of the web to get your problem solved instantly and have answers to your questions immediately!.</p>
-            <form action="#" method="post">
+            <form action="#" onSubmit={this.onSubmit}>
               <div class="form-group first">
                 <label for="username">Username</label>
-                <input type="text" class="form-control" placeholder="your-email@gmail.com" id="username"/>
+                <input type="text" name="username" value={this.state.username} onChange={this.onChange} className="form-control" placeholder="your-email@gmail.com" />
               </div>
               <div class="form-group last mb-3">
                 <label for="password">Password</label>
-                <input type="password" class="form-control" placeholder="Your Password" id="password"/>
+                <input type="password" value={this.state.password} onChange={this.onChange} class="form-control" name="password" placeholder="Your Password" id="password"/>
               </div>
               
               <div class="d-flex mb-5 align-items-center">
@@ -44,9 +65,10 @@ class App extends Component{
                 </Link>
               </div>
 
-              <Link to="/home">
+              {/* Routing is commented intentionally because it will be opened after validating */}
+              {/* <Link to="/home"> */}
                 <input type="submit" value="Log In" class="btn btn-block btn-primary"/>
-              </Link>
+              {/* </Link> */}
 
             </form>
           </div>
@@ -55,6 +77,7 @@ class App extends Component{
     </div>
 
     
+  </div>
   </div>
 
     )
